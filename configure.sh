4d8ca247398d10df12e35ea68d328ed9f5b5d2be
Praveen Ramanujam <praveen.ramanujam@yahoo.com>
2014-03-20 11:03:46 +0100

    Added workspace explorer

--- configure.sh
+++ configure.sh
@@ -1,5 +1,6 @@
 sudo apt-get install git git-core cmake libboost-all-dev python-dev python-numpy xdotool qt-sdk libxml2-dev libboost-thread-dev libboost-date-time-dev libboost-filesystem-dev libboost-system-dev libboost-regex-dev
 git clone https://github.com/praman2s/youbot_driver.git
+git clone https://github.com/praman2s/youbot-workspace-explorer.git
 cd youbot_driver
 mkdir build
 cd build
